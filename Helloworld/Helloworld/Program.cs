a9be55327299f5db9cb441d428a58cccc1ff2874
Rajneesh Kumar <97rajnish@gmail.com>
2019-07-17 18:03:59 +0530

    first commit

---
+++ Helloworld/Helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print();
+        }
+        public static void Print()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
