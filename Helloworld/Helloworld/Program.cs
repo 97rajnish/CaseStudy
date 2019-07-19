b1e2312c54a42b835e35ec1f5b3c27f64c90b8fd
sanjay.ganta <gantasanjay143@gmail.com>
2019-07-19 14:19:38 +0530

    Popup window added

--- Helloworld/Helloworld/Program.cs
+++ Helloworld/Helloworld/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Helloworld
 {
@@ -10,11 +12,25 @@ namespace Helloworld
     {
         static void Main(string[] args)
         {
-            Print();
+            string inputString = "Hello World";
+            StringWriter stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            Print(inputString);
+            StringBuilder str = stringWriter.GetStringBuilder();
+            string consoleOutput = str.ToString();
+            consoleOutput = consoleOutput.Replace(System.Environment.NewLine, string.Empty);
+            stringWriter.Close();
+            if (string.Equals(consoleOutput, inputString))
+                MessageBox.Show("Execution Successful");
+            else
+                MessageBox.Show("Execution UnSuccessful");
+
+
         }
-        public static void Print()
+        public static void Print(string input)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine(input);
+
         }
     }
 }
